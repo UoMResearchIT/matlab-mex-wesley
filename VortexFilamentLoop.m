9a953baba8c561b73447d881ac86bf069b363fd5
Gerard Capes <gerard.capes@manchester.ac.uk>
2021-09-16 15:05:40 +0100

    Define inputs for VortexFilamentLoop

--- VortexFilamentLoop.m
+++ VortexFilamentLoop.m
@@ -1,5 +1,13 @@
 function V = VortexFilamentLoop(P,A,B,Gamma,rc) %#codegen
-    
+    % Define intput parameter types for codegen
+    assert(isa(P, 'double'))
+    assert(isa(A, 'double'))
+    assert(isa(B, 'double'))
+    assert(isa(Gamma, 'double'))
+    assert(isa(rc, 'double'))
+    assert(size(A, 1) == 3)
+    assert(size(B, 1) == 3)
+
     % number of filaments
     Nfils = length(Gamma);
     % preallocate the velocity from each vortex filament
